670edafb095f6d1e5038f9f68cce512cb287107c
Petar Yankov <petar741012@gmail.com>
2022-08-17 13:48:47 +0300

    Update streamlit_app.py
    Added emojies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@
 import streamlit
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Acocado toast')
+ \ \ 🥑🍞
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
